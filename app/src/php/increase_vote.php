728d38826f97f324be9b3247287940103245d6fa
“chagrdigit config --global user.name “Charlotte Grandjean <charlotte.grandjean@yahoo.fr>
2019-03-07 16:19:47 +0100

    d

--- app/src/php/increase_vote.php
+++ app/src/php/increase_vote.php
@@ -1,14 +1,18 @@
 <?php
 // Connect to database
-$db = new PDO('mysql:host=localhost;dbname="arcade","root","root"');
+$db = new PDO('mysql:host=localhost;dbname=arcade','root','root');
 
 // Request to database (get nb_votes)
 $q = $db->prepare("SELECT * FROM vote WHERE slug_game = :game");
-$q->bindäraù(":game", $_GET["game"]);
+$q->bindParam(":game", $_GET["game"]);
 $q->execute();
 
 $nb_votes = $q->fetch(PDO::FETCH_ASSOC);
 
+$nb_votes = $nb_votes["nb_votes"] +1;
+
 var_dump($nb_votes);
+
+$q = $db->prepare("UPDATE vote SET nb_votes = :nb_votes WHERE slug_games = :game");
 
 ?>
