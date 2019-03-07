47e33707e7520cc9db256561a80ea17bd3f1e484
Sarah <sarahprs@icloud.com>
2019-03-07 16:13:37 +0100

    d

--- app/src/php/increase_vote.php
+++ app/src/php/increase_vote.php
@@ -7,9 +7,9 @@ $q = $db->prepare("SELECT * FROM vote WHERE slug_game = :game");
 $q->bindParam(":game", $_GET["game"]);
 $q->execute();
 
-$nb_vote = $q->fetch(PDO::FETCH_ASSOC);
+$nb_votes = $q->fetch(PDO::FETCH_ASSOC);
 
-$nb_votes = $nb_vote["nb_votes"] +1;
+$nb_votes = $nb_votes["nb_votes"] +1;
 
 var_dump($nb_votes);
 
