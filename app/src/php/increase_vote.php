7917bd375eb0d183936097330fac68eba94e9a94
Sarah <sarahprs@icloud.com>
2019-03-07 16:03:11 +0100

    d

--- app/src/php/increase_vote.php
+++ app/src/php/increase_vote.php
@@ -4,10 +4,12 @@ $db = new PDO('mysql:host=localhost;dbname="arcade","root","root"');
 
 // Request to database (get nb_votes)
 $q = $db->prepare("SELECT * FROM vote WHERE slug_game = :game");
-$q->bindäraù(":game", $_GET["game"]);
+$q->bindParam(":game", $_GET["game"]);
 $q->execute();
 
 $nb_votes = $q->fetch(PDO::FETCH_ASSOC);
+
+$nb-votes = $nb_vote["nb_votes"]++;
 
 var_dump($nb_votes);
 
