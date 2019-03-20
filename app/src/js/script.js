e5a385e1639d7b0c7e54cf51e1dc1897cbe32b72
“chagrdigit config --global user.name “Charlotte Grandjean <charlotte.grandjean@yahoo.fr>
2019-03-20 14:28:15 +0100

    swag

--- app/src/js/script.js
+++ app/src/js/script.js
@@ -58,7 +58,7 @@ window.onclick = function(event) {
 }
 
 
-/* ANISS */
+/* GABRIEL */
 
 // Get the modal
 var gabriel = document.getElementById('gabrielModal');
@@ -67,7 +67,7 @@ var gabriel = document.getElementById('gabrielModal');
 var btn3 = document.getElementById("gabrielBtn");
 
 // Get the <span> element that closes the modal
-var span3 = document.getElementsByClassName("close3")[0];
+var span3 = document.getElementsByClassName("close6")[0];
 
 // When the user clicks on the button, open the modal 
 btn3.onclick = function() {
@@ -86,6 +86,144 @@ window.onclick = function(event) {
   }
 }
 
+/* SEBASTIAN */
+
+// Get the modal
+var sebastian = document.getElementById('sebastianModal');
+
+// Get the button that opens the modal
+var btn4 = document.getElementById("sebastianBtn");
+
+// Get the <span> element that closes the modal
+var span4 = document.getElementsByClassName("close4")[0];
+
+// When the user clicks on the button, open the modal 
+btn4.onclick = function() {
+  sebastian.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span4.onclick = function() {
+  sebastian.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    sebastian.style.display = "none";
+  }
+}
+
+/* CLARA */
+
+// Get the modal
+var clara = document.getElementById('claraModal');
+
+// Get the button that opens the modal
+var btn5 = document.getElementById("claraBtn");
+
+// Get the <span> element that closes the modal
+var span5 = document.getElementsByClassName("close3")[0];
+
+// When the user clicks on the button, open the modal 
+btn5.onclick = function() {
+  clara.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span5.onclick = function() {
+  clara.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    clara.style.display = "none";
+  }
+}
 
 
+/* NICOLAS */
 
+// Get the modal
+var nicolas = document.getElementById('nicolasModal');
+
+// Get the button that opens the modal
+var btn6 = document.getElementById("nicolasBtn");
+
+// Get the <span> element that closes the modal
+var span6 = document.getElementsByClassName("close5")[0];
+
+// When the user clicks on the button, open the modal 
+btn6.onclick = function() {
+  nicolas.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span6.onclick = function() {
+  nicolas.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    nicolas.style.display = "none";
+  }
+}
+
+
+/* SIMON */
+
+// Get the modal
+var simon = document.getElementById('simonModal');
+
+// Get the button that opens the modal
+var btn7 = document.getElementById("simonBtn");
+
+// Get the <span> element that closes the modal
+var span7 = document.getElementsByClassName("close7")[0];
+
+// When the user clicks on the button, open the modal 
+btn7.onclick = function() {
+  simon.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span7.onclick = function() {
+  simon.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    simon.style.display = "none";
+  }
+}
+
+/* CHARLELIE */
+
+// Get the modal
+var charlelie = document.getElementById('sebastianModal');
+
+// Get the button that opens the modal
+var btn4 = document.getElementById("sebastianBtn");
+
+// Get the <span> element that closes the modal
+var span4 = document.getElementsByClassName("close4")[0];
+
+// When the user clicks on the button, open the modal 
+btn4.onclick = function() {
+  sebastian.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span4.onclick = function() {
+  sebastian.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    sebastian.style.display = "none";
+  }
+}
