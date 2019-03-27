9fe7f120dcc707e5906069533277d84343444814
Soraya <sorayaazm@gmail.com>
2019-03-27 14:18:51 +0100

    buttons

--- app/src/js/script.js
+++ app/src/js/script.js
@@ -123,7 +123,7 @@ span11.onclick = function() {
 }
 
 /*FACEBOOK BUTTON*/
-(function(d, s, id) {
+  (function(d, s, id) {
     var js, fjs = d.getElementsByTagName(s)[0];
     if (d.getElementById(id)) return;
     js = d.createElement(s); js.id = id;
@@ -132,7 +132,23 @@ span11.onclick = function() {
   }(document, 'script', 'facebook-jssdk'));
 
 
-
+/*API TWITTER*/
+  window.twttr = (function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0],
+      t = window.twttr || {};
+    if (d.getElementById(id)) return t;
+    js = d.createElement(s);
+    js.id = id;
+    js.src = "https://platform.twitter.com/widgets.js";
+    fjs.parentNode.insertBefore(js, fjs);
+  
+    t._e = [];
+    t.ready = function(f) {
+      t._e.push(f);
+    };
+  
+    return t;
+  }(document, "script", "twitter-wjs"));
 
 
 
